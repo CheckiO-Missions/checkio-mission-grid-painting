aafdae8c60ecaf457cd3402c78908d9edb1efe94
kurosawa4434 <kurosawa4434@gmail.com>
2023-11-21 23:22:54 +0900

    cut random-tests

--- verification/tests.py
+++ verification/tests.py
@@ -6,57 +6,7 @@ Each test is a dict with
     "answer" -- your right answer
     "explanation" -- not necessarily a key, it's used for an additional info in animation.
 """
-from itertools import combinations, combinations_with_replacement, chain
-from string import ascii_uppercase as au
-from random import sample, randint
-
-
-def grid_painting(cells: str) -> tuple[int, list[list[str]]]:
-    def area(c1, c2):
-        (y1, x1), (y2, x2) = c1, c2
-        return [(y, x) for y in range(min(y1, y2), max(y1, y2) + 1) for x in range(min(x1, x2), max(x1, x2) + 1)]
-
-    def all_shapes(coords):
-        for cs in combinations_with_replacement(coords, 2):
-            shape = area(*cs)
-            if set(shape) <= set(coords):
-                yield shape
-
-    coords = [divmod(au.index(a), 5) for a in cells]
-
-    shapes = list(all_shapes(coords))
-    last_shapes = set()
-    for sp1 in shapes:
-        for sp2 in shapes:
-            if set(sp1) < set(sp2):
-                break
-        else:
-            last_shapes.add(frozenset(sp1))
-
-    for n in range(1, len(last_shapes)+1):
-        for pieces in combinations(last_shapes, n):
-            s = set(chain(*pieces))
-            if set(coords) <= set(s):
-                rectangles = []
-                for p in pieces:
-                    sp = sorted(p)
-                    y1, x1 = min(sp)
-                    y2, x2 = max(sp)
-                    rectangles.append([au[y1 * 5 + x1], au[y2 * 5 + x2]])
-                return n, rectangles
-
-
-def make_raondom_tests(num: int) -> list[dict]:
-    for _ in range(num):
-        cells = ''.join(sorted(sample(au[:25], randint(4, 24))))
-        answer, explanation = grid_painting(cells)
-        yield {"input": [cells],
-               "answer": answer,
-               "explanation": explanation}
-
-
 TESTS = {
-    "Randoms": list(make_raondom_tests(5)),
     "Basics": [
         {
             "input": ["ABCFGHMRX"],
